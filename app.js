940c605f3d94d7485465f5fb494bc8bf272f0dcb
187107363 <43198174+187107363@users.noreply.github.com>
2019-04-03 15:58:07 +0800

    Tut completed

---
+++ app.js
@@ -0,0 +1,34 @@
+const Express = require("express");
+const BodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+
+const CONNECTION_URL = "mongodb+srv://chengcm:HG25m2gf5HXtWd63@304cem-assignment-c3cpk.azure.mongodb.net/test?retryWrites=true";
+const DATABASE_NAME = "example";
+
+var app = Express();
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+
+var database, collection;
+
+app.post("/person", (request, response) => {
+    collection.insert(request.body, (error, result) => {
+        if (error) {
+            return response.status(500).send(error);
+        }
+        response.send(result.result);
+    });
+});
+
+app.listen(3000, () => {
+    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+        if (error) {
+            throw error;
+        }
+        database = client.db(DATABASE_NAME);
+        collection = database.collection("people");
+        console.log("Connected to `" + DATABASE_NAME + "`!");
+    });
+});
