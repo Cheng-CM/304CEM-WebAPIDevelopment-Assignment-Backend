281cdb8e51367f9935607a20161eb533bfd24a63
187107363 <43198174+187107363@users.noreply.github.com>
2019-04-09 16:46:47 +0800

    CRUD Raffle

--- models/raffle.js
+++ models/raffle.js
@@ -4,13 +4,16 @@ var mongoose = require('mongoose');
 //Define a schema
 var Schema = mongoose.Schema;
 
-var UserSchema = new Schema({
-    username: String,
-    password: String,
-    email: String,
-    created: { type: Date, default: Date.now() }
+var RaffleSchema = new Schema({
+    name: String,
+    description: String,
+    createdBy: Schema.Types.ObjectId,
+    created: { type: Date, default: Date.now() },
+    item: Schema.Types.ObjectId,
+    joined: [Schema.Types.ObjectId],
+    active: Boolean
 });
 
-//Export function to create "User" model class
-var User = mongoose.model('User', UserSchema);
-module.exports = User;
+//Export function to create "Raffle" model class
+var Raffle = mongoose.model('Raffle', RaffleSchema);
+module.exports = Raffle;
